be093815688f7a40cf4aba46d025d29096f296ce
Sarah Walker <sarahsweetpie6@gmail.com>
2025-03-11 15:11:55 -0600

    Created models for 3 of the 5 database table

---
+++ GrowApp/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GrowApp.Models
+{
+    public class Post
+    {
+        [Key]
+        [Required]
+        public int post_id { get; set; }
+        public int user_id { get; set; }
+        public string photo { get; set; }
+        public string caption { get; set; }
+        public string posted_at { get; set; }
+
+        [ForeignKey('goal_id')]
+        public int goal_id { get; set; }
+        public Goal Goal { get; set; }
+    }
+}
