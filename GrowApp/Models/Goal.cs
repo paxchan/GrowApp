cea6d9e80a9bfc99fda48943b213dcaa4cc32ab7
summersampson <summersampson57@gmail.com>
2025-03-12 10:14:04 -0600

    trying to fix issues

---
+++ GrowApp/Models/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GrowApp.Models
+{
+    public class Goal
+    {
+
+        [Key]
+        [Required]
+        public int goal_id { get; set; }
+
+        [ForeignKey("user_id")]
+        public int user_id { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        public string title { get; set; }
+        public string description { get; set; }
+
+        public string start_date { get; set; }
+        public string end_date { get; set; }
+
+        public string status { get; set; }
+
+        public bool visibility { get; set; }
+
+        public string created_at { get; set; }
+
+        public int frequency { get; set; }
+
+
+    }
+}
